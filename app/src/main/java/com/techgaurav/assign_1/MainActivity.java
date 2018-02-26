5793c42f62553eef62f53c15bfff637a188875ab
gaurav <gaurav.adhikari36@gmail.com>
2018-02-26 14:53:18 +0530

    initial commit

---
+++ app/src/main/java/com/techgaurav/assign_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.techgaurav.assign_1;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.cent_layout);
+
+
+
+    }
+}
